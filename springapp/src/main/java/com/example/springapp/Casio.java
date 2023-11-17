aa2da9746b5339c088f1b4481fc7e6e35c64484d
Iamneo <techadmin@iamneo.ai>
2023-11-17 06:04:56 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/Casio.java
+++ springapp/src/main/java/com/example/springapp/Casio.java
@@ -4,4 +4,28 @@ public class Casio implements Calculator{
    //Write Your code Here
 
 
+   int sum=obj.add();
+   int diff=obj.sub();
+   System.out.println(sum);
+   System.out.println(diff);
+   int sum=obj.add();
+   int diff=obj.sub();
+   System.out.println(sum);
+   System.out.println(diff);
+   int sum=obj.add();
+   int diff=obj.sub();
+   System.out.println(sum);
+   System.out.println(diff);
+   int sum=obj.add();
+   int diff=obj.sub();
+   System.out.println(sum);
+   System.out.println(diff);
+   int sum=obj.add();
+   int diff=obj.sub();
+   System.out.println(sum);
+   System.out.println(diff);
+   int sum=obj.add();
+   int diff=obj.sub();
+   System.out.println(sum);
+   System.out.println(diff);
 }
